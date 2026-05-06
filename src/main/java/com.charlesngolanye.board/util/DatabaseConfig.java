79c97fa9064bf4f7f1a77e0c68a0b2d6ac913468
charles <charles.ngolanye@gmail.com>
2026-05-06 16:25:32 +0200

    Update DatabaseConfig

--- src/main/java/com.charlesngolanye.board/util/DatabaseConfig.java
+++ src/main/java/com.charlesngolanye.board/util/DatabaseConfig.java
@@ -5,9 +5,9 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DatabaseConfig {
-    private static final String URL = "jdbc:mysql://localhost:3306/board";
-    private static final String USER = "root";
-    private static final String PASSWORD = ".NET8fowler";
+    private static final String URL = "";
+    private static final String USER = "";
+    private static final String PASSWORD = "";
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
